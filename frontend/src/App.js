e7b7ecc5de2cbd0e71af069b7463b91158ae2031
OXCY01 <yousif2002@berkeley.edu>
2022-06-07 21:53:17 -0700

    new

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">amazon</a>
+      </header>
+      <main>list products here</main>
+    </div>
+  );
+}
+
+export default App;
